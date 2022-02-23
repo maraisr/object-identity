bc2b93345f995c9360638345fa5e9f845ba93514
Marais Rossouw <me@marais.co>
2022-02-23 10:33:50 +1000

    :construction: WIP

--- index.ts
+++ index.ts
@@ -1,48 +1,64 @@
-function walk(input: any, seen: WeakSet<any>) {
-	if (input == null || typeof input !== 'object') return input;
-	if (seen.has(input)) return '{CIRCULAR}';
-	seen.add(input);
-
-	let out = '', i = 0, tmp: unknown, keys: string[];
-
-	switch (Object.prototype.toString.call(input)) {
-		case '[object Set]':
-		case '[object Array]': {
-			out += 'a';
-			for (tmp of input) out += walk(tmp, seen);
-			return out;
-		}
-
-		case '[object Object]': {
-			out += 'o';
-			keys = Object.keys(input).sort();
-			for (i = 0; i < keys.length; i++) {
-				tmp = keys[i];
-				out += tmp + walk(input[tmp as string], seen);
-			}
-			return out;
-		}
-
-		case '[object Map]': {
-			out += 'o';
-			keys = Array.from((input as Map<string, unknown>).keys()).sort();
-			for (tmp of keys) out += tmp + walk(input.get(tmp), seen);
-			return out;
-		}
-
-		case '[object Date]':
-			return out + 'd' + +input;
-
-		case '[object RegExp]':
-			return out + 'r' + input.source + input.flags;
-
-		default:
-			throw new Error(`Unsupported value ${input}`);
-	}
-}
-
 export type Hasher = (input: string) => any;
 
 export function identify<T, H extends Hasher>(input: T, hasher: H): ReturnType<H> {
-	return hasher(walk(input, new WeakSet));
+	const seen = new WeakSet;
+
+	let out = '', i = 0, tmp: unknown, item: any, keys: string[];
+
+	let bucket: any[] = [input];
+	loop: while (item = bucket.pop()!) {
+		if (item == null || typeof item !== 'object') {
+			out += input;
+			continue;
+		} else if (seen.has(item)) {
+			out += '{CIRCULAR}';
+			continue;
+		}
+		seen.add(item);
+
+		switch (Object.prototype.toString.call(item)) {
+			case '[object Set]':
+			case '[object Array]': {
+				out += 'a';
+				for (tmp of item) bucket.push(tmp);
+				break;
+			}
+
+			case '[object Object]': {
+				out += 'o';
+				keys = Object.keys(item).sort();
+				for (i = 0; i < keys.length; i++) {
+					tmp = keys[i]; // key
+					out += tmp;
+					bucket.push(item[tmp as string]); // value
+				}
+				break;
+			}
+
+			case '[object Map]': {
+				out += 'o';
+				keys = Array.from((item as Map<string, unknown>).keys()).sort();
+				for (tmp of keys) {
+					out += tmp; // key
+					bucket.push(item.get(tmp)); // value
+				}
+				break;
+			}
+
+			case '[object Date]': {
+				out + 'd' + +item;
+				break;
+			}
+
+			case '[object RegExp]': {
+				out + 'r' + item.source + item.flags;
+				break;
+			}
+
+			default:
+				throw new Error(`Unsupported value ${item}`);
+		}
+	}
+
+	return hasher(out);
 }
